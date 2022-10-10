6679320e3dd099e4d49f800d5c549feb89926c05
Kapturov Alexander <86742866+Ultraluxe25@users.noreply.github.com>
2022-10-10 22:49:55 +0300

    beautified self code

--- Intermediate Python/Shooting Game.py
+++ Intermediate Python/Shooting Game.py
@@ -25,26 +25,28 @@ The while loop stops when the user enters 'exit'.
 """
 
 class Enemy:
-  name = ""
-  lives = 0
-  def __init__(self, name, lives):
-    self.name = name
-    self.lives = lives
+    name = ""
+    lives = 0
+    def __init__(self, name, lives):
+        self.name = name
+        self.lives = lives
 
-  def hit(self):
-    self.lives -= 1
-    if self.lives <= 0:
-       print(self.name + ' killed')
-    else:
-        print(self.name + ' has '+ str(self.lives) + ' lives')
+    def hit(self):
+        self.lives -= 1
+        if self.lives <= 0:
+            print(self.name + ' killed')
+        else:
+            print(self.name + ' has '+ str(self.lives) + ' lives')
 
+            
 class Monster(Enemy):
-  def __init__(self):
-    super().__init__('Monster', 3)
+    def __init__(self):
+        super().__init__('Monster', 3)
 
+        
 class Alien(Enemy):
-  def __init__(self):
-    super().__init__('Alien', 5)
+    def __init__(self):
+        super().__init__('Alien', 5)
 
 
 m = Monster()
